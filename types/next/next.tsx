61b72c04e62c81e3639871df440872fa5aac9a57
Jonathan Corral <johnlejardinnier@gmail.com>
2021-11-24 19:47:28 +0100

    fix(types/next): update type NextPageProps

--- types/next/next.tsx
+++ types/next/next.tsx
@@ -3,8 +3,10 @@ import type { NextPage as DefaultNextPage } from 'next';
 import type { AppProps as DefaultAppProps } from 'next/app';
 
 export type NextPageProps = {
-  layout?: React.ComponentType;
+  pageLayout?: React.ComponentType;
+  rootProps?: Object;
   name?: string;
+  title?: string;
 };
 
 export type NextPage<P = {}, IP = P> = DefaultNextPage<P, IP> & NextPageProps;
