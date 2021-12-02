e5a2ccc1d909069633489aae6387d50543f466ae
Jonathan Corral <johnlejardinnier@gmail.com>
2021-12-02 18:03:32 +0100

    feat(lib/api): create lib api component

---
+++ lib/api/api.tsx
@@ -0,0 +1,11 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import nc, { Options as NextConnectOptions } from 'next-connect';
+
+/**
+ * Helper function to create API handlers.
+ *
+ * @param options handler creation options
+ */
+export function createApiHandler(options?: NextConnectOptions<NextApiRequest, NextApiResponse>) {
+  return nc<NextApiRequest, NextApiResponse>(options);
+}
---
+++ lib/api/index.ts
@@ -0,0 +1,1 @@
+export { createApiHandler } from './api';
